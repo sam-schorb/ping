55e0f53bbd4f04e37af95193aa623f5f968aa5f3
Sam Schorb <sam_schorb@hotmail.com>
2026-04-17 13:21:27 +0100

    Stop tracking local test artifacts

--- packages/core/test/helpers/build-fixtures.js
+++
@@ -1,8 +0,0 @@
-import { readFile } from "node:fs/promises";
-
-const FIXTURE_ROOT = new URL("../fixtures/build/", import.meta.url);
-
-export async function loadBuildFixture(name) {
-  const fixtureText = await readFile(new URL(name, FIXTURE_ROOT), "utf8");
-  return JSON.parse(fixtureText);
-}
--- packages/core/test/helpers/routing-fixtures.js
+++
@@ -1,9 +0,0 @@
-import { readFile } from "node:fs/promises";
-
-const FIXTURE_ROOT = new URL("../fixtures/routing/", import.meta.url);
-
-export async function loadRoutingFixture(name) {
-  const fixtureText = await readFile(new URL(name, FIXTURE_ROOT), "utf8");
-
-  return JSON.parse(fixtureText);
-}
